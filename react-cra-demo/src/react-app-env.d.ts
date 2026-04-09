ae7d568d4ce56302a5ecb3bb312cf4525f728143
黄柄权 <bingquan111@qq.com>
2026-04-09 21:00:44 +0800

    feat: add vite react-ts template

--- react-cra-demo/src/react-app-env.d.ts
+++ react-cra-demo/src/react-app-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="react-scripts" />
+
+// 手动声明 CSS 文件类型
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: { [className: string]: string };
+  export default content;
+}
